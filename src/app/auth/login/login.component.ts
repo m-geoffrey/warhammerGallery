906330f905d3e4683f1fd996398c57897d77bcae
m-geoffrey <moniez.geoffrey@yahoo.fr>
2020-07-20 00:39:57 +0200

    fix component and routerLink

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  passwordShow = false;
+  faEye = faEye;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  passwordDisplay(): void {
+    console.log('password display function');
+    this.passwordShow = ! this.passwordShow;
+    console.log ('show password: ' + this.passwordShow);
+  }
 }
