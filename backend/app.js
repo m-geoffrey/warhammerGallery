ced9fabae688a06859ba425d49899be007592e92
m-geoffrey <moniez.geoffrey@yahoo.fr>
2020-07-22 19:55:56 +0200

    update form signup & must-match-validators

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,45 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
+const User = require("./models/user");
 
 const app = express();
 
+mongoose
+  .connect(
+    "mongodb+srv://hitbox:"
+    + process.env.MONGO_ATLAS_PW
+    + "@cluster0.jlrds.gcp.mongodb.net/miniature-warhammer?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log('Connected to database !');
+  })
+  .catch(() => {
+    console.log('Connection failed !');
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-  console.log('first middleware');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
+
 
 app.use((req, res, next) => {
   res.send('hello from express');
 });
 
 module.exports = app;
+
+// 4sa4FhDEc2wupBGa
